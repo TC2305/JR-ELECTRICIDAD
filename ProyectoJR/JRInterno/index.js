5ac318dae60222ac14ea7682ecc27059e1bd2059
TC2305 <tcascante@edu.uc.ac.cr>
2026-04-29 00:20:45 -0600

    Add files via upload

--- ProyectoJR/JRInterno/index.js
+++ ProyectoJR/JRInterno/index.js
@@ -1,9 +1,22 @@
-
 import { supabase } from "./supabase.js";
 
-const { data: { session } } = await supabase.auth.getSession();
+document.addEventListener("DOMContentLoaded", async () => {
+  // Verificar sesión
+  const { data: { session } } = await supabase.auth.getSession();
 
-if (!session) {
-  // No hay login → vuelve al login
-  window.location.href = "login.html";
-}
+  // Si no hay sesión 
+  if (!session) {
+    window.location.href = "login.html";
+    return; // detener ejecución
+  }
+
+  const btnLogout = document.getElementById("btnLogout");
+
+  if (btnLogout) {
+    btnLogout.addEventListener("click", async (e) => {
+      e.preventDefault();
+      await supabase.auth.signOut();
+      window.location.href = "login.html";
+    });
+  }
+});
